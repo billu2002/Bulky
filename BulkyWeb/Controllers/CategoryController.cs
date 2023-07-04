d924ac084cfab4d7a281fcfa9cd688262b97cbbe
billu2002 <94391016+billu2002@users.noreply.github.com>
2023-07-04 12:17:32 +0500

    All CRUD operations for the category have been implemented

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -21,5 +21,78 @@ namespace BulkyWeb.Controllers
 		{
 			return View();
 		}
+		[HttpPost]
+		public IActionResult Create(Category obj)
+		{
+			if (obj.Name == obj.DisplayOrder.ToString())
+			{
+				ModelState.AddModelError("name", "The Display Order cannot exaclty match the name");
+			}
+			if ( obj.Name!=null && obj.Name.ToLower() == "test")
+			{
+				ModelState.AddModelError("", "Test is an invalid value");
+			}
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Add(obj);
+				_db.SaveChanges();
+				TempData["success"] = "Category created succefully";
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+		public IActionResult Edit(int? id)
+		{
+			if(id==null || id ==0)
+			{
+				return NotFound();
+			}
+			Category categoryFromDb = _db.Categories.Find(id);
+			if(categoryFromDb == null) 
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+		}
+		[HttpPost]
+		public IActionResult Edit(Category obj)
+		{
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Update(obj);
+				_db.SaveChanges();
+				TempData["success"] = "Category updated succefully";
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category categoryFromDb = _db.Categories.Find(id);
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+		}
+		[HttpPost,ActionName("Delete")]
+		public IActionResult DeletePOST(int? id)
+		{
+			Category? obj = _db.Categories.Find(id);
+			if(obj == null)
+			{
+				return NotFound();
+			}
+			_db.Categories.Remove(obj);
+		    _db.SaveChanges();
+			TempData["success"] = "Category deleted succefully";
+			return RedirectToAction("Index");
+		}
 	}
 }
