4f2bef76277742850234478f595e2c01a067cb98
billu2002 <94391016+billu2002@users.noreply.github.com>
2023-07-07 00:23:36 +0500

    Shopping cart has been implemented

--- Bulky.DataAccess/Repository/Repository.cs
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -6,7 +6,9 @@ using System.Text;
 using System.Threading.Tasks;
 using Bulky.DataAccess.Repository.IRepository;
 using Bulky.DataAcess.Data;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace Bulky.DataAccess.Repository
 {
@@ -28,25 +30,41 @@ namespace Bulky.DataAccess.Repository
 			dbSet.Add(entity);
 		}
 
-		public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null)
+		public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null,
+			bool tracked = false)
 		{
-			IQueryable<T> query = dbSet;
-			query= query.Where(filter);
-			if (!string.IsNullOrEmpty(includeProperties))
+			IQueryable<T> query;
+
+            if (tracked)
 			{
-				foreach (var includeProp in includeProperties
-					.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
-				{
-					query = query.Include(includeProp);
-				}
+				query = dbSet;
+			
 			}
-			return query.FirstOrDefault();
-		}
+			else
+			{
+                query = dbSet.AsNoTracking();
+              
+            }
+				query = query.Where(filter);
+				if (!string.IsNullOrEmpty(includeProperties))
+				{
+					foreach (var includeProp in includeProperties
+						.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+					{
+						query = query.Include(includeProp);
+					}
+				}
+				return query.FirstOrDefault();
+        }
 
 		// Category, CoverType
-		public IEnumerable<T> GetAll(string? includeProperties = null)
+		public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter, string? includeProperties = null)
 		{
 			IQueryable<T> query = dbSet;
+			if (filter != null)
+			{
+				query = query.Where(filter);
+			}
 			if(!string.IsNullOrEmpty(includeProperties))
 			{
 				foreach(var includeProp in includeProperties
